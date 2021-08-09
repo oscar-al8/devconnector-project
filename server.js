0382dd906d45b1c94eb4d01369c7a414b09db1f3
oscar_al8 <oscarmehrer@hotmail.com>
2021-08-09 12:44:23 -0400

    user API routes and JWT authentication created

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
